643c34823d6266b79f7c366dd2e84038775f40ce
gabrielaugz <95982237+gabrielaugz@users.noreply.github.com>
2025-06-18 18:21:01 -0300

    added

--- frontend/src/api/petfinder/index.js
+++ frontend/src/api/petfinder/index.js
@@ -1,6 +1,6 @@
 // src/api/petfinder/index.js
 
-const API = 'http://localhost:3001/api';
+const API = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
 
 async function fetchJSON(url) {
   const res = await fetch(url);
