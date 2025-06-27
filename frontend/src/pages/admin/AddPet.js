8f653b81a55e6124da63d0f53574242b58c83f50
Gabriel ALS. Castro <u13717212@exatas.ffclrp.usp.br>
2025-06-26 22:27:48 -0300

    correção pagina de adicionar pet

--- frontend/src/pages/admin/AddPet.js
+++ frontend/src/pages/admin/AddPet.js
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { API_BASE } from '../../utils/api'
-import './AddPet.css'
 
 export default function AddPet() {
   const navigate = useNavigate()
