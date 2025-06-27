5822257c7af8aef06098cbad96f2527da3abaac4
Gabriel ALS. Castro <u13717212@exatas.ffclrp.usp.br>
2025-06-26 22:46:14 -0300

    correção pagina de adicionar pet

--- frontend/src/pages/admin/EditPet.js
+++ frontend/src/pages/admin/EditPet.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { API_BASE } from '../../utils/api'
-import './EditPet.css'
+
 
 export default function EditPet() {
   const { id } = useParams()
