ddfddef17b87c71aaae4db15886dfc6aa902d61f
Gabriel ALS. Castro <u13717212@exatas.ffclrp.usp.br>
2025-06-26 23:11:30 -0300

    correção topo de pagina

--- frontend/src/components/root/index.js
+++ frontend/src/components/root/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Navigation from '../navigation';
 import { Outlet } from 'react-router-dom';
 import Footer from "../footer";
-import ScrollToTop from '../components/ScrollToTop';
 
 const Root = () => {
     return (
         <>
-            <ScrollToTop />
             <Navigation/>
             <Outlet />
             <Footer/>
